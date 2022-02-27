ec82aa96b7856c3921ef58e7f9f81060b58de4ad
blackPavlin <boikiv.bogdan@gmail.com>
2022-02-27 23:48:31 +0400

    Add interlacing benchmarks

--- benchmarks/png/decoder/index.bench.ts
+++ benchmarks/png/decoder/index.bench.ts
@@ -3,22 +3,39 @@ import path from 'path';
 import { Suite, Event } from 'benchmark';
 import Decoder from '../../../src/png/decoder';
 
-// basn0g01 x 19,649 ops/sec ±0.79% (93 runs sampled)
-// basn0g02 x 17,238 ops/sec ±0.31% (91 runs sampled)
-// basn0g04 x 12,969 ops/sec ±0.32% (95 runs sampled)
-// basn0g08 x 20,008 ops/sec ±7.80% (76 runs sampled)
-// basn0g16 x 19,036 ops/sec ±3.71% (91 runs sampled)
-// basn2c08 x 15,740 ops/sec ±6.41% (82 runs sampled)
-// basn2c16 x 10,083 ops/sec ±2.32% (92 runs sampled)
-// basn3p01 x 19,259 ops/sec ±0.28% (95 runs sampled)
-// basn3p02 x 15,970 ops/sec ±2.58% (92 runs sampled)
-// basn3p04 x 12,196 ops/sec ±0.26% (95 runs sampled)
-// basn3p08 x 14,225 ops/sec ±3.49% (91 runs sampled)
-// basn4a08 x 19,147 ops/sec ±7.96% (78 runs sampled)
-// basn4a16 x 11,721 ops/sec ±4.09% (92 runs sampled)
-// basn6a08 x 17,607 ops/sec ±5.77% (63 runs sampled)
-// basn6a16 x 6,983 ops/sec ±4.37% (73 runs sampled)
+// basn0g01 x 18,500 ops/sec ±2.22% (89 runs sampled)
+// basn0g02 x 15,581 ops/sec ±2.06% (87 runs sampled)
+// basn0g04 x 11,916 ops/sec ±1.23% (89 runs sampled)
+// basn0g08 x 18,865 ops/sec ±8.13% (73 runs sampled)
+// basn0g16 x 19,493 ops/sec ±3.49% (90 runs sampled)
+// basn2c08 x 15,624 ops/sec ±6.56% (80 runs sampled)
+// basn2c16 x 10,087 ops/sec ±3.18% (89 runs sampled)
+// basn3p01 x 19,877 ops/sec ±1.04% (90 runs sampled)
+// basn3p02 x 16,693 ops/sec ±2.64% (88 runs sampled)
+// basn3p04 x 12,645 ops/sec ±0.70% (93 runs sampled)
+// basn3p08 x 14,859 ops/sec ±0.96% (93 runs sampled)
+// basn4a08 x 19,445 ops/sec ±8.09% (75 runs sampled)
+// basn4a16 x 13,215 ops/sec ±2.29% (95 runs sampled)
+// basn6a08 x 18,102 ops/sec ±5.87% (64 runs sampled)
+// basn6a16 x 6,994 ops/sec ±4.82% (71 runs sampled)
 
+// basi0g01 x 5,779 ops/sec ±0.51% (92 runs sampled)
+// basi0g02 x 5,887 ops/sec ±0.31% (95 runs sampled)
+// basi0g04 x 5,198 ops/sec ±2.49% (93 runs sampled)
+// basi0g08 x 6,537 ops/sec ±0.29% (93 runs sampled)
+// basi0g16 x 6,227 ops/sec ±0.38% (95 runs sampled)
+// basi2c08 x 6,207 ops/sec ±2.17% (95 runs sampled)
+// basi2c16 x 5,129 ops/sec ±0.58% (93 runs sampled)
+// basi3p01 x 5,933 ops/sec ±0.26% (95 runs sampled)
+// basi3p02 x 5,673 ops/sec ±1.79% (94 runs sampled)
+// basi3p04 x 5,105 ops/sec ±0.34% (94 runs sampled)
+// basi3p08 x 5,523 ops/sec ±2.52% (94 runs sampled)
+// basi4a08 x 6,408 ops/sec ±0.29% (95 runs sampled)
+// basi4a16 x 5,043 ops/sec ±2.54% (95 runs sampled)
+// basi6a08 x 6,540 ops/sec ±0.27% (93 runs sampled)
+// basi6a16 x 4,547 ops/sec ±0.31% (93 runs sampled)
+
+// No-Interlacing
 const basn0g01 = fs.readFileSync(path.join(__dirname, './images/basn0g01.png'));
 const basn0g02 = fs.readFileSync(path.join(__dirname, './images/basn0g02.png'));
 const basn0g04 = fs.readFileSync(path.join(__dirname, './images/basn0g04.png'));
@@ -34,6 +51,23 @@ const basn4a08 = fs.readFileSync(path.join(__dirname, './images/basn4a08.png'));
 const basn4a16 = fs.readFileSync(path.join(__dirname, './images/basn4a16.png'));
 const basn6a08 = fs.readFileSync(path.join(__dirname, './images/basn6a08.png'));
 const basn6a16 = fs.readFileSync(path.join(__dirname, './images/basn6a16.png'));
+
+// Interlacing
+const basi0g01 = fs.readFileSync(path.join(__dirname, './images/basi0g01.png'));
+const basi0g02 = fs.readFileSync(path.join(__dirname, './images/basi0g02.png'));
+const basi0g04 = fs.readFileSync(path.join(__dirname, './images/basi0g04.png'));
+const basi0g08 = fs.readFileSync(path.join(__dirname, './images/basi0g08.png'));
+const basi0g16 = fs.readFileSync(path.join(__dirname, './images/basi0g16.png'));
+const basi2c08 = fs.readFileSync(path.join(__dirname, './images/basi2c08.png'));
+const basi2c16 = fs.readFileSync(path.join(__dirname, './images/basi2c16.png'));
+const basi3p01 = fs.readFileSync(path.join(__dirname, './images/basi3p01.png'));
+const basi3p02 = fs.readFileSync(path.join(__dirname, './images/basi3p02.png'));
+const basi3p04 = fs.readFileSync(path.join(__dirname, './images/basi3p04.png'));
+const basi3p08 = fs.readFileSync(path.join(__dirname, './images/basi3p08.png'));
+const basi4a08 = fs.readFileSync(path.join(__dirname, './images/basi4a08.png'));
+const basi4a16 = fs.readFileSync(path.join(__dirname, './images/basi4a16.png'));
+const basi6a08 = fs.readFileSync(path.join(__dirname, './images/basi6a08.png'));
+const basi6a16 = fs.readFileSync(path.join(__dirname, './images/basi6a16.png'));
 
 const suite = new Suite('Png decoder - no-interlacing');
 
@@ -52,6 +86,22 @@ suite.add('basn4a08', () => new Decoder(basn4a08));
 suite.add('basn4a16', () => new Decoder(basn4a16));
 suite.add('basn6a08', () => new Decoder(basn6a08));
 suite.add('basn6a16', () => new Decoder(basn6a16));
+
+suite.add('basi0g01', () => new Decoder(basi0g01));
+suite.add('basi0g02', () => new Decoder(basi0g02));
+suite.add('basi0g04', () => new Decoder(basi0g04));
+suite.add('basi0g08', () => new Decoder(basi0g08));
+suite.add('basi0g16', () => new Decoder(basi0g16));
+suite.add('basi2c08', () => new Decoder(basi2c08));
+suite.add('basi2c16', () => new Decoder(basi2c16));
+suite.add('basi3p01', () => new Decoder(basi3p01));
+suite.add('basi3p02', () => new Decoder(basi3p02));
+suite.add('basi3p04', () => new Decoder(basi3p04));
+suite.add('basi3p08', () => new Decoder(basi3p08));
+suite.add('basi4a08', () => new Decoder(basi4a08));
+suite.add('basi4a16', () => new Decoder(basi4a16));
+suite.add('basi6a08', () => new Decoder(basi6a08));
+suite.add('basi6a16', () => new Decoder(basi6a16));
 
 suite.on('cycle', (event: Event) => {
 	console.log(String(event.target));
--- benchmarks/png/decoder/pngjs.bench.ts
+++ benchmarks/png/decoder/pngjs.bench.ts
@@ -3,22 +3,39 @@ import path from 'path';
 import { Suite, Event } from 'benchmark';
 import { PNG } from 'pngjs';
 
-// basn0g01 x 9,419 ops/sec ±0.65% (91 runs sampled)
-// basn0g02 x 9,966 ops/sec ±0.29% (94 runs sampled)
-// basn0g04 x 9,740 ops/sec ±0.17% (96 runs sampled)
-// basn0g08 x 32,691 ops/sec ±0.19% (93 runs sampled)
-// basn0g16 x 8,758 ops/sec ±0.29% (96 runs sampled)
-// basn2c08 x 16,096 ops/sec ±3.03% (95 runs sampled)
-// basn2c16 x 5,975 ops/sec ±0.35% (94 runs sampled)
-// basn3p01 x 9,967 ops/sec ±2.84% (92 runs sampled)
-// basn3p02 x 9,902 ops/sec ±1.06% (87 runs sampled)
-// basn3p04 x 9,806 ops/sec ±2.57% (94 runs sampled)
-// basn3p08 x 18,194 ops/sec ±0.31% (95 runs sampled)
-// basn4a08 x 23,790 ops/sec ±2.56% (93 runs sampled)
-// basn4a16 x 5,748 ops/sec ±0.27% (95 runs sampled)
-// basn6a08 x 14,690 ops/sec ±2.97% (91 runs sampled)
-// basn6a16 x 4,505 ops/sec ±0.22% (96 runs sampled)
+// basn0g01 x 7,656 ops/sec ±21.65% (81 runs sampled)
+// basn0g02 x 8,830 ops/sec ±1.45% (92 runs sampled)
+// basn0g04 x 8,888 ops/sec ±1.56% (87 runs sampled)
+// basn0g08 x 32,535 ops/sec ±0.65% (94 runs sampled)
+// basn0g16 x 8,866 ops/sec ±0.53% (95 runs sampled)
+// basn2c08 x 16,111 ops/sec ±2.41% (93 runs sampled)
+// basn2c16 x 6,023 ops/sec ±0.71% (95 runs sampled)
+// basn3p01 x 10,066 ops/sec ±2.74% (93 runs sampled)
+// basn3p02 x 10,167 ops/sec ±0.45% (95 runs sampled)
+// basn3p04 x 9,953 ops/sec ±2.46% (95 runs sampled)
+// basn3p08 x 18,271 ops/sec ±0.82% (95 runs sampled)
+// basn4a08 x 23,833 ops/sec ±2.03% (95 runs sampled)
+// basn4a16 x 5,795 ops/sec ±0.17% (98 runs sampled)
+// basn6a08 x 13,700 ops/sec ±2.78% (93 runs sampled)
+// basn6a16 x 4,544 ops/sec ±0.14% (98 runs sampled)
 
+// basi0g01 x 7,185 ops/sec ±3.78% (88 runs sampled)
+// basi0g02 x 7,597 ops/sec ±2.72% (95 runs sampled)
+// basi0g04 x 7,505 ops/sec ±0.09% (98 runs sampled)
+// basi0g08 x 16,031 ops/sec ±2.72% (94 runs sampled)
+// basi0g16 x 6,808 ops/sec ±0.10% (97 runs sampled)
+// basi2c08 x 12,947 ops/sec ±2.71% (96 runs sampled)
+// basi2c16 x 5,032 ops/sec ±0.66% (95 runs sampled)
+// basi3p01 x 7,741 ops/sec ±3.33% (95 runs sampled)
+// basi3p02 x 7,962 ops/sec ±0.57% (98 runs sampled)
+// basi3p04 x 7,808 ops/sec ±2.13% (92 runs sampled)
+// basi3p08 x 12,847 ops/sec ±0.47% (95 runs sampled)
+// basi4a08 x 14,484 ops/sec ±1.70% (95 runs sampled)
+// basi4a16 x 4,976 ops/sec ±0.13% (97 runs sampled)
+// basi6a08 x 10,844 ops/sec ±2.86% (96 runs sampled)
+// basi6a16 x 3,954 ops/sec ±0.08% (98 runs sampled)
+
+// No-Interlacing
 const basn0g01 = fs.readFileSync(path.join(__dirname, './images/basn0g01.png'));
 const basn0g02 = fs.readFileSync(path.join(__dirname, './images/basn0g02.png'));
 const basn0g04 = fs.readFileSync(path.join(__dirname, './images/basn0g04.png'));
@@ -34,6 +51,23 @@ const basn4a08 = fs.readFileSync(path.join(__dirname, './images/basn4a08.png'));
 const basn4a16 = fs.readFileSync(path.join(__dirname, './images/basn4a16.png'));
 const basn6a08 = fs.readFileSync(path.join(__dirname, './images/basn6a08.png'));
 const basn6a16 = fs.readFileSync(path.join(__dirname, './images/basn6a16.png'));
+
+// Interlacing
+const basi0g01 = fs.readFileSync(path.join(__dirname, './images/basi0g01.png'));
+const basi0g02 = fs.readFileSync(path.join(__dirname, './images/basi0g02.png'));
+const basi0g04 = fs.readFileSync(path.join(__dirname, './images/basi0g04.png'));
+const basi0g08 = fs.readFileSync(path.join(__dirname, './images/basi0g08.png'));
+const basi0g16 = fs.readFileSync(path.join(__dirname, './images/basi0g16.png'));
+const basi2c08 = fs.readFileSync(path.join(__dirname, './images/basi2c08.png'));
+const basi2c16 = fs.readFileSync(path.join(__dirname, './images/basi2c16.png'));
+const basi3p01 = fs.readFileSync(path.join(__dirname, './images/basi3p01.png'));
+const basi3p02 = fs.readFileSync(path.join(__dirname, './images/basi3p02.png'));
+const basi3p04 = fs.readFileSync(path.join(__dirname, './images/basi3p04.png'));
+const basi3p08 = fs.readFileSync(path.join(__dirname, './images/basi3p08.png'));
+const basi4a08 = fs.readFileSync(path.join(__dirname, './images/basi4a08.png'));
+const basi4a16 = fs.readFileSync(path.join(__dirname, './images/basi4a16.png'));
+const basi6a08 = fs.readFileSync(path.join(__dirname, './images/basi6a08.png'));
+const basi6a16 = fs.readFileSync(path.join(__dirname, './images/basi6a16.png'));
 
 const suite = new Suite('Pngjs - no-interlacing');
 
@@ -52,6 +86,22 @@ suite.add('basn4a08', () => PNG.sync.read(basn4a08));
 suite.add('basn4a16', () => PNG.sync.read(basn4a16));
 suite.add('basn6a08', () => PNG.sync.read(basn6a08));
 suite.add('basn6a16', () => PNG.sync.read(basn6a16));
+
+suite.add('basi0g01', () => PNG.sync.read(basi0g01));
+suite.add('basi0g02', () => PNG.sync.read(basi0g02));
+suite.add('basi0g04', () => PNG.sync.read(basi0g04));
+suite.add('basi0g08', () => PNG.sync.read(basi0g08));
+suite.add('basi0g16', () => PNG.sync.read(basi0g16));
+suite.add('basi2c08', () => PNG.sync.read(basi2c08));
+suite.add('basi2c16', () => PNG.sync.read(basi2c16));
+suite.add('basi3p01', () => PNG.sync.read(basi3p01));
+suite.add('basi3p02', () => PNG.sync.read(basi3p02));
+suite.add('basi3p04', () => PNG.sync.read(basi3p04));
+suite.add('basi3p08', () => PNG.sync.read(basi3p08));
+suite.add('basi4a08', () => PNG.sync.read(basi4a08));
+suite.add('basi4a16', () => PNG.sync.read(basi4a16));
+suite.add('basi6a08', () => PNG.sync.read(basi6a08));
+suite.add('basi6a16', () => PNG.sync.read(basi6a16));
 
 suite.on('cycle', (event: Event) => {
 	console.log(String(event.target));
